91eecb93eb7bf78ed0ebf8babd810646c49918ee
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 00:04:01 -0400

    add login header and form to login page

--- public/Page.js
+++ public/Page.js
@@ -1,1 +1,16 @@
-var body = document.querySelector("body");
+const body = document.querySelector("body");
+
+const header1 = document.createElement("h1");
+var node = document.createTextNode("Login");
+header1.appendChild(node);
+body.appendChild(header1);
+
+var form = document.createElement("form");
+body.appendChild(form);
+const LoginForm = document.querySelector("form");
+
+const input = document.createElement("input");
+for (let index = 0; index < 2; index++) {
+  console.log([index]);
+  LoginForm.append(input);
+}
