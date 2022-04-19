d894acbd44309420ccc0ff9a6b1f3e9b52c8b316
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 23:24:33 -0400

    change components to modules

---
+++ public/modules/signUp.js
@@ -0,0 +1,10 @@
+
+
+const createRequest = function (url, succeed, fail) {
+  fetch(url)
+    .then((response) => handleErrors(response))
+    .then((data) => succeed(data))
+    .catch((error) => fail(error));
+};
+
+createRequest(url, updateUISuccess, updateUIError);
