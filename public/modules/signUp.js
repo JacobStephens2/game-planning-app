7c4428c14004276c781054c0be70f0e89561c8ba
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 23:58:28 -0400

    get signUp.js module to fetch

--- public/modules/signUp.js
+++ public/modules/signUp.js
@@ -1,3 +1,20 @@
 import { apiHostname } from '/modules/apiHostname.js';
+import { handleErrors } from '/modules/handleErrors.js';
 
-console.log(apiHostname);
+const updateUISuccess = function (data) {
+  console.log(JSON.parse(data));
+};
+
+const updateUIError = function (error) {
+  console.log(error);
+  return error;
+};
+
+const createRequest = function (url, succeed, fail) {
+  fetch(url)
+    .then((response) => handleErrors(response))
+    .then((data) => succeed(data))
+    .catch((error) => fail(error));
+};
+
+createRequest(apiHostname, updateUISuccess, updateUIError);
