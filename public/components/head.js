27dce3c236670d02344e5412eb181cd15674682a
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 22:07:18 -0400

    make head reusable and break css into various files

---
+++ public/components/head.js
@@ -0,0 +1,13 @@
+const headInnerHTML = `
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign Up</title>
+  <link rel="stylesheet" href="style/remedy.css">
+  <link rel="stylesheet" href="style/reminders.css">
+  <link rel="stylesheet" href="style/style.css">
+`;
+
+const head = document.querySelector('head');
+
+head.innerHTML = headInnerHTML;
