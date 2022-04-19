381ed323c2f8dcfb1d08f798f4c65678537b288d
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-19 00:02:36 -0400

    move createRequest up in signUp.js

--- public/modules/signUp.js
+++ public/modules/signUp.js
@@ -1,6 +1,13 @@
 import { apiHostname } from '/modules/apiHostname.js';
 import { handleErrors } from '/modules/handleErrors.js';
 
+const createRequest = function (url, succeed, fail) {
+  fetch(url)
+    .then(response => handleErrors(response))
+    .then(data => succeed(data))
+    .catch(error => fail(error));
+};
+
 const updateUISuccess = function (data) {
   console.log(JSON.parse(data));
 };
@@ -8,13 +15,6 @@ const updateUISuccess = function (data) {
 const updateUIError = function (error) {
   console.log(error);
   return error;
-};
-
-const createRequest = function (url, succeed, fail) {
-  fetch(url)
-    .then(response => handleErrors(response))
-    .then(data => succeed(data))
-    .catch(error => fail(error));
 };
 
 createRequest(apiHostname, updateUISuccess, updateUIError);
