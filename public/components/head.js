65543419709700e3f935d014ddcf4d1943cb2782
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 22:08:42 -0400

    shorten head.js

--- public/components/head.js
+++ public/components/head.js
@@ -1,4 +1,4 @@
-const headInnerHTML = `
+document.querySelector('head').innerHTML = `
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,3 @@ const headInnerHTML = `
   <link rel="stylesheet" href="style/reminders.css">
   <link rel="stylesheet" href="style/style.css">
 `;
-
-const head = document.querySelector('head');
-
-head.innerHTML = headInnerHTML;
