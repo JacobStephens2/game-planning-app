12a139baf0bb833781bce8ea780e281dbe14dca4
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-18 23:03:26 -0400

    add apiURL to footer

--- public/components/footer.js
+++ public/components/footer.js
@@ -1,3 +1,8 @@
+apiURL = (window.location.origin == 'https://gameplanning.site') ?
+  'https://api.gameplanning.site' :
+  'http://api.gameplanning.local';
+
 document.querySelector('footer').innerHTML = `
   <a href="https://github.com/JacobStephens2/game-planning-front-end">GitHub</a>
+  <a href="${apiURL}">API</a>
 `;
