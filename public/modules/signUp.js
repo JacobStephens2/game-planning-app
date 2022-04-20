9dd834ab48055765c52d147153bc32393bf3eb04
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-19 21:32:09 -0400

    POST signup request to sign-up.php endpoint

--- public/modules/signUp.js
+++ public/modules/signUp.js
@@ -1,20 +1,23 @@
 import { apiHostname } from '/modules/apiHostname.js';
-import { handleErrors } from '/modules/handleErrors.js';
 
-const createRequest = function (url, succeed, fail) {
-  fetch(url)
-    .then(response => handleErrors(response))
-    .then(data => succeed(data))
-    .catch(error => fail(error));
-};
+let signUpEndpoint = apiHostname + 'sign-up.php';
 
-const updateUISuccess = function (data) {
-  console.log(JSON.parse(data));
-};
-
-const updateUIError = function (error) {
-  console.log(error);
-  return error;
-};
-
-createRequest(apiHostname, updateUISuccess, updateUIError);
+document.querySelector('input[type="submit"]').addEventListener('click', function (event) {
+  event.preventDefault()
+  let email = document.querySelector('input[type="email"]').value;
+  let password = document.querySelector('input[type="password"]').value;
+  let data = {};
+  data.email = email;
+  data.password = password;
+  fetch(signUpEndpoint, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+});
