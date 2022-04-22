01661fe04bfa98aadcd330ec220ad0806381b623
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-22 00:45:13 -0400

    adjust login to be similar to sign up

--- public/modules/login.js
+++ public/modules/login.js
@@ -1,26 +1,37 @@
 import { apiHostname } from '/modules/apiHostname.js';
 import { handleErrors } from '/modules/handleErrors.js';
+import { updateUIError } from '/modules/updateUIError.js';
+
+let signUpEndpoint = apiHostname + 'login.php';
 
 document.querySelector('input[type="submit"]').addEventListener('click', function (event) {
   event.preventDefault()
+  let data = {};
+  data.email = document.querySelector('input[type="email"]').value;
+  data.password = document.querySelector('input[type="password"]').value;
+
+  const updateUISuccess = function (data) {
+    const parsedData = JSON.parse(data);
+    var message = parsedData.message;
+    let paragraph = document.createElement('p');
+    paragraph.innerText = parsedData;
+    document.querySelector('body').append(paragraph);
+  }
+
+  const createRequest = function (url, succeed, fail) {
+    fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => handleErrors(response))
+      .then((data) => succeed(data))
+      .catch((error) => fail(error));
+  };
+
+  createRequest(signUpEndpoint, updateUISuccess, updateUIError);
+
 });
-
-let requestURL = apiHostname + 'login.php';
-
-const createRequest = function (url, succeed, fail) {
-  fetch(url)
-    .then(response => handleErrors(response))
-    .then(data => succeed(data))
-    .catch(error => fail(error));
-};
-
-const updateUISuccess = function (data) {
-  console.log(JSON.parse(data));
-};
-
-const updateUIError = function (error) {
-  console.log(error);
-  return error;
-};
-
-createRequest(requestURL, updateUISuccess, updateUIError);
