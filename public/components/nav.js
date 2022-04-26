3d5d4a9338359396c83fcd66110b4891262517fe
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-26 00:37:07 -0400

    add logout function to nav for cookie

--- public/components/nav.js
+++ public/components/nav.js
@@ -1,11 +1,28 @@
 function logout() {
-  localStorage.removeItem('loggedIn');
-  window.location = window.location;
+  document.cookie = "loggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  window.location = "/views/login.html";
 }
 
-if (localStorage.getItem('loggedIn') == 'true') {
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie('loggedIn') == 'true') {
   document.querySelector('nav').innerHTML = `
   <a href="/index.html">Game Planning App</a>
+  <a href="/views/games.html">Games</a>
   <button onclick="logout()">Logout</a>
   `;
 } else {
