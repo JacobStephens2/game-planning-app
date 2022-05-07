41844390997b875d15c6a2c595804acf8ad2405e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-07 00:48:58 -0400

    add games page for logged in users

--- modules/games.js
+++ modules/games.js
@@ -1,7 +1,32 @@
 import { cookieMethods } from "./cookieMethods";
+import { apiHostname } from '/modules/apiHostname.js';
+import { handleErrors } from '/modules/handleErrors.js';
+import { updateUIError } from '/modules/updateUIError.js';
 
-if (!cookieMethods.getCookie('loggedIn')) {
-  window.location;
+if (cookieMethods.getCookie('loggedIn') == 'true') {
+  console.log('You are logged in');
 } else {
-  console.log('logged in');
+  window.location = '/login';
 }
+
+let signUpEndpoint = apiHostname + 'games';
+
+const createRequest = function (url, succeed, fail) {
+  fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then((response) => handleErrors(response))
+    .then((data) => succeed(data))
+    .catch((error) => fail(error));
+};
+
+const updateUISuccess = function (data) {
+  const parsedData = JSON.parse(data);
+  document.querySelector('span+span').innerText = parsedData.email;
+}
+
+createRequest(signUpEndpoint, updateUISuccess, updateUIError);
